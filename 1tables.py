76608c6237247bd16c1c6825a43100366146c6f3
Shruti Shekhar <shrutishekhar1996@gmail.com>
2026-04-08 18:55:43 +0530

    4T

--- 1tables.py
+++ 1tables.py
@@ -29,3 +29,11 @@ for i in range(1,11):
         print(f"{a} X {i} = {product}, even")
     else:
         print(f"{a} X {i} = {product}, Odd")
+
+a = 5
+for i in range(1,11):
+    product = a * i
+    if product % 2 == 1:
+        print(f"{a} X {i} = {product}, odd")
+    else:
+        print(f"{a} X {i} = {product}, even")
